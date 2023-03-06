56f59209b29a8338a37e9015aad2283ab2d3fa8c
David <muriithid05@gmail.com>
2023-03-06 11:59:06 +0000

    Terms and Policy pages. Tested the CountyDropdown

---
+++ src/pages/PrivacyPolicy.js
@@ -0,0 +1,89 @@
+import React from 'react'
+
+function PrivacyPolicy() {
+    return (
+        <>
+            <section className='bg-gray-100 p-5 md:p-12 lg:p-16'>
+                <div className='text-center text-lg font-semibold'><h1>Our Privacy Policy at HouseForRentKE</h1></div>
+                <div><h2 className='text-lg py-4 font-semibold'>Privacy Policy</h2>
+
+                    <span>Effective date: April 1st 2023</span>
+
+                    <p>
+                        We at HouseForRentKE respect your privacy and are committed to protecting it through our compliance with this privacy policy.
+
+                        This Policy describes the types of information we may collect from you or that you may provide when you use our website and our practices for collecting, using, maintaining, protecting, and disclosing that information.
+
+                        Please read this Policy carefully to understand our policies and practices regarding your information and how we will treat it. If you do not agree with our policies and practices, your choice is not to use our Website. By accessing or using this Website, you agree to this Policy. This Policy may change from time to time, and your continued use of the Website after we make changes is deemed to be acceptance of those changes, so please check the Policy periodically for updates.
+                    </p>
+                    <h2 className='text-lg py-4 font-semibold'>Information We Collect:</h2>
+
+                    <p>We collect several types of information from and about users of our Website, including:
+
+                        Information you provide to us: This includes information you provide when you register for an account, search for a rental property, post a rental listing, or contact us for support or information. The information may include your name, email address, phone number, payment information, and any other information you choose to provide.
+
+                        Information we collect automatically: When you use the Website, we may automatically collect certain information about your device, including your IP address, browser type, operating system, referring URLs, pages visited, and the dates and times of visits to the Website.
+
+                        Information we collect from third-party sources: We may receive information about you from third-party sources, such as social media platforms or other websites, that you have authorized to share your information with us.
+                    </p>
+                    <h2 className='text-lg py-4 font-semibold'>How We Use Your Information</h2>
+
+                    <span>We may use the information we collect from and about you for various purposes, including:</span>
+
+                    <ul className='list-disc list-inside'>
+                        <li>To provide you with the services and features offered on the Web</li>
+
+                        <li>To personalize your experience on the Website.</li>
+
+                        <li>To communicate with you about your account, rentals, or listings.</li>
+
+                        <li>To improve the Website and our services.</li>
+
+                        <li>To comply with legal obligations, such as responding to court orders or subpoenas.</li>
+
+                        <li>To protect our rights and property.</li>
+                    </ul>
+                    <span>We may also use your information to send you marketing communications or newsletters, unless you opt out of receiving them.</span>
+
+                    <h2 className='text-lg py-4 font-semibold'>How We Share Your Information:</h2>
+
+                    <span>We may share your information with third parties in the following circumstances:</span>
+
+                    <ul className='list-disc list-inside'>With service providers and contractors who help us provide the Website and its services.
+
+                        <li>With landlords or tenants who use the Website to post or search for rentals.</li>
+
+                        <li>With third-party advertisers and advertising networks who help deliver our ads to you.</li>
+
+                        <li>With our affiliates and subsidiaries.</li>
+
+                        <li>To comply with legal obligations, such as responding to court orders or subpoenas.</li>
+                    </ul>
+                    <span>We may also share your information with your consent or at your direction.</span>
+
+                    <h2 className='text-lg py-4 font-semibold'>Your Choices:</h2>
+
+                    <span>You may have the following choices regarding your information:</span>
+                    <ul className='list-disc list-inside'>
+                        <li>You can review and update your account information by logging in to your account on the Website.</li>
+
+                        <li>You can opt out of receiving marketing communications or newsletters by following the instructions in the communication.</li>
+
+                        <li>You can delete your account by contacting us at [Insert email address].</li>
+
+                        <li>You can disable cookies in your browser settings, although this may limit your ability to use certain features of the Website.</li>
+                    </ul>
+                    <h2 className='text-lg py-4 font-semibold font-semibold'>Data Security</h2>
+
+                    <span>We have implemented measures to secure your information from accidental loss and from unauthorized access, use, alteration, and disclosure. However, no method of transmission over the internet or method of electronic storage is 100% secure. Therefore, we cannot guarantee the absolute security of your information.</span>
+
+                    <h2 className='text-lg py-4 font-semibold'>Children's Privacy</h2>
+
+                    <span>The Website is not intended for people under 13 years of age. We DO NOT knowingly collect personal information from children</span>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default PrivacyPolicy;
---
+++ src/pages/TermsOfService.js
@@ -0,0 +1,119 @@
+import React from 'react'
+
+function TermsOfService() {
+    return (
+        <>
+            <section className='bg-gray-100 p-5 md:p-12 lg:p-16'>
+                <div className=''>
+                    <h1 className='text-xl font-semibold py-4 text-center'>Terms of Service</h1>
+                    <h1 className='text-xl font-semibold py-4'>Agreement to Terms</h1>
+                    <p>
+                        These Terms of Service constitute a legally binding agreement made between you, whether personally or on behalf of an entity ("you") and [website owner] ("we," "us" or "our"), concerning your access to and use of the [website name] website as well as any other media form, media channel, mobile website or mobile application related, linked, or otherwise connected thereto (collectively, the "Site").
+                    </p>
+                    <p>
+                        You agree that by accessing the Site, you have read, understood, and agreed to be bound by all of these Terms of Service. If you do not agree with all of these Terms of Service, then you are expressly prohibited from using the Site and you must discontinue use immediately.
+                    </p>
+
+                    <h1 className='text-xl font-semibold py-4'>User Representations</h1>
+                    By using the Site, you represent and warrant that:
+                    <ul className='list-disc list-inside'>
+                        <li>All registration information you submit will be true, accurate, current, and complete</li>
+                        <li>You will maintain the accuracy of such information and promptly update such registration information as necessary</li>
+                        <li>You have the legal capacity and you agree to comply with these Terms of Service</li>
+                        <li>You are not a minor in the jurisdiction in which you reside</li>
+                    </ul>
+                    <h1 className='text-xl font-semibold py-4'>User Registration</h1>
+                    <p>
+                        You may be required to register with the Site. You agree to keep your password confidential and will be responsible for all use of your account and password. We reserve the right to remove, reclaim, or change a username you select if we determine, in our sole discretion, that such username is inappropriate, obscene, or otherwise objectionable.
+                    </p>
+                    <h1 className='text-xl font-semibold py-4'>Prohibited Activities</h1>
+                    You may not access or use the Site for any purpose other than that for which we make the Site available. Prohibited activity includes, but is not limited to:
+
+                    <ul className='list-disc list-inside'>
+                        <li>Attempting to bypass any measures of the Site designed to prevent or restrict access to the Site, or any portion of the Site</li>
+                        <li>Attempting to impersonate another user or person or using the username of another user</li>
+                        <li>Criminal or tortious activity</li>
+                        <li>Engaging in any automated use of the system, such as using scripts to send comments or messages</li>
+                        <li>Interfering with, disrupting, or creating an undue burden on the Site or the networks or services connected to the Site</li>
+                        <li>Posting or transmitting any unlawful, harmful, threatening, abusive, harassing, defamatory, vulgar, obscene, sexually explicit, profane, hateful, racially, ethnically, or otherwise objectionable content</li>
+                        <li>Using the Site to upload, transmit, or distribute any computer viruses, worms, or any software intended to damage or alter a computer system or data</li>
+                        <li>Soliciting, collecting, or harvesting any personally identifiable information, including account names, from the Site</li>
+                        <li>Using the Site to advertise or offer to sell goods and services</li>
+                    </ul>
+                    <h1 className='text-xl font-semibold py-4'>Intellectual Property Rights</h1>
+                    The Site and its entire contents, features, and functionality (including but not limited to all information, software, text, displays, images, video, and audio, and the design, selection, and arrangement thereof), are owned by [website owner], its licensors, or other providers of such material and are protected by United States and international copyright, trademark, patent, trade secret, and other intellectual property or proprietary rights laws.
+
+                    These Terms of Service permit you to use the Site for your personal, non-commercial use only. You must not reproduce, distribute, modify, create derivative works of, publicly display, publicly perform, republish, download, store, or transmit any of the material on our Site, except as follows:
+
+                    <ul className='list-disc list-inside'>
+                        <li>Your computer may temporarily store copies of such materials in RAM incidental to your accessing and viewing those materials</li>
+                        <li>You may store files that are automatically cached by your Web browser for display enhancement purposes</li>
+                        <li>You may print or download one copy of a reasonable number of pages of the Site for your own personal, non-commercial use and not for further reproduction, publication, or distribution
+                            If we provide desktop, mobile, or other applications for download, you may download a single copy to your computer or mobile device solely for your own personal, non-commercial use, and not for further reproduction, publication, or distribution. If we provide social media features with certain content, you may take such actions as are enabled by such features.
+                        </li>
+                    </ul>
+                    <h1 className='text-xl font-semibold py-4'>User-Generated Content</h1>
+                    <p>
+                        The Site may invite you to chat or participate in blogs, message boards, online forums, and other functionality, and may provide you with the opportunity to create, submit, post, display, transmit, perform, publish, distribute, or broadcast content and materials to us or on the Site, including but not limited to text, writings, video, audio, photographs, graphics, comments, suggestions, or personal information or other material (collectively, "Contributions").
+                    </p>
+
+                    <p>
+                        Contributions may be viewable by other users of the Site and through third-party websites. As such, any Contributions you transmit may be treated as non-confidential and non-proprietary. When you create or make available any Contributions, you thereby represent and warrant that:
+                    </p>
+
+                    <p>
+                        The creation, distribution, transmission, public display, or performance, and the accessing, downloading, or copying of your Contributions do not and will not infringe the proprietary rights, including but not limited to the copyright, patent, trademark, trade secret, or moral rights of any third party.
+                    </p>
+                    <p>
+                        You are the creator and owner of or have the necessary licenses, rights, consents, releases, and permissions to use and to authorize us, the Site, and other users of the Site to use your Contributions in any manner contemplated by the Site and these Terms of Service
+                        You have the written consent, release, and/or permission of each and every identifiable individual person in your Contributions to use their name or likeness to enable inclusion and use of your Contributions in any manner contemplated by the Site and these Terms of Service
+                        Monitoring and Enforcement;
+                    </p>
+                    <h1 className='text-xl font-semibold py-4'>Termination</h1>
+                    <span>We have the right to:</span>
+
+                    <ul className='list-disc list-inside'>
+                        <li>Remove or refuse to post any Contributions for any or no reason in our sole discretion.</li>
+                        <li>Take any action with respect to any Contribution that we deem necessary or appropriate in our sole discretion, including if we believe that such Contribution violates the Terms of Service, infringes any intellectual property right or other right of any person or entity, threatens the personal safety of users of the Site or the public, or could create liability for the Site or us
+                            Disclose your identity or other information about you to any third party who claims that material posted by you violates their rights, including their intellectual property rights or their right to privacy
+                        </li>
+                        <li>Take appropriate legal action, including without limitation, referral to law enforcement, for any illegal or unauthorized use of the Site.</li>
+                        <li>
+                            Terminate or suspend your access to all or part of the Site for any or no reason, including without limitation, any violation of these Terms of Service
+                            Without limiting the foregoing, we have the right to fully cooperate with any law enforcement authorities or court order requesting or directing us to disclose the identity or other information of anyone posting any materials on or through the Site. You waive and hold harmless the Site and its affiliates, licensees, and service providers from any claims resulting from any action taken by any of the foregoing parties during, or taken as a consequence of, investigations by either such parties or law enforcement authorities.
+                        </li>
+                    </ul>
+                    <h1 className='text-xl font-semibold py-4'>Disclaimer</h1>
+                    <p>
+                        The Site is provided on an "as-is" and "as available" basis, and the Company and our suppliers expressly disclaim any warranties and conditions of any kind, whether express or implied, including the warranties or conditions of merchantability, fitness for a particular purpose, title, quiet enjoyment, accuracy, or non-infringement. We and our suppliers make no warranty that the Site will meet your requirements, will be available on an uninterrupted, timely, secure, or error-free basis, or will be accurate, reliable, free of viruses or other harmful code, complete, legal, or safe. If applicable law requires any warranties with respect to the Site, all such warranties are limited in duration to ninety (90) days from the date of first use.
+                    </p>
+                    <p>
+                        Some jurisdictions do not allow the exclusion of implied warranties, so the above exclusion may not apply to you. Some jurisdictions do not allow limitations on how long an implied warranty lasts, so the above limitation may not apply to you.
+                    </p>
+                    <h1 className='text-xl font-semibold py-4'>Limitation on Liability</h1>
+                    <p>
+                        To the maximum extent permitted by law, in no event shall the Company or our suppliers be liable to you or any third-party for any lost profits, lost data, costs of procurement of substitute products, or any indirect, consequential, exemplary, incidental, special or punitive damages arising from or relating to these Terms of Service or your use of, or incapability to use the Site even if the Company has been advised of the possibility of such damages. Access to and use of the Site is at your own discretion and risk, and you will be solely responsible for any damage to your device or computer system, or loss of data resulting therefrom.
+                    </p>
+                    <p>
+                        To the maximum extent permitted by law, notwithstanding anything to the contrary contained herein, our liability to you for any damages arising from or related to this agreement, will at all times be limited to a maximum of fifty U.S. dollars (USD$50). The existence of more than one claim will not enlarge this limit. You agree that our suppliers will have no liability of any kind arising from or relating to this agreement.
+                    </p>
+                    <h1 className='text-xl font-semibold py-4'>Indemnification</h1>
+                    <p>
+                        You agree to indemnify and hold the Company and our affiliates, directors, officers, employees, and agents harmless from and against any liabilities, losses, damages, or costs, including reasonable attorneys' fees, incurred in connection with or arising from any third-party allegations, claims, actions, disputes, or demands asserted against any of them as a result of or relating to your Content, your use of the Site or Services, or any willful misconduct on your part.
+                    </p>
+                    <h1 className='text-xl font-semibold py-4'>Assignment</h1>
+                    <p>
+                        These Terms of Service, and any rights and licenses granted hereunder, may not be transferred or assigned by you, but may be assigned by the Company without restriction.
+                    </p>
+
+                    <h1 className='text-xl font-semibold py-4'>Governing Law; Jurisdiction</h1>
+                    <p>
+                        These Terms of Service shall be governed by and construed in accordance with the laws of the Republic of Kenya, without giving effect to any principles of conflicts of law. You agree that any action at law or in equity arising out of or relating to these Terms of Service or the Site shall be filed only in the state or federal courts located in the Republic of Kenya, and you hereby irrevocably and unconditionally consent and submit to the exclusive jurisdiction of such courts over any suit, action, or proceeding arising out of these Terms of Service.
+                    </p>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default TermsOfService;
